05b28ee5a64263d22fee6f158c11a4572dc7bb8a
jahre <devnull@localhost>
2008-08-14 15:53:14 +0000

    nå regner jeg også ut shared/alone ratioene

--- fairmha/evaluateInterference.py
+++ fairmha/evaluateInterference.py
@@ -2,9 +2,13 @@
 import sys
 import pbsconfig
 import re
+import deterministic_fw_wls as fair_wls
+import single_core_fw as single_wls
 
 bmPattern = re.compile("-EBENCHMARK=[a-zA-Z0-9]*")
 startPattern = re.compile("^[0-9]:")
+ipcPattern = re.compile("detailedCPU[0-9].COM:IPC.*")
+idPattern = re.compile("[0-9]+")
 
 np = 4
 banks = 4
@@ -13,6 +17,17 @@ def getBenchmark(cmd):
     res = bmPattern.findall(cmd)
     bm = res[0].split('=')[1]
     return bm
+
+def getBMFromKey(id):
+    idSplit = id.split('_')
+    return idSplit[1]
+
+def getKeyFromID(id):
+    idSplit = id.split('_')
+    restStr = idSplit[2]
+    for n in idSplit[3:]:
+        restStr = restStr + "_" + n
+    return restStr
 
 def getMatrix(infile):
     matrix = []
@@ -31,6 +46,67 @@ def getMatrix(infile):
         if findCnt == np:
             return matrix
 
+def getIPCs(resIDs, results, useKey):
+    for resID in resIDs:
+        bm = getBMFromKey(resID)
+        wlNum = int(idPattern.findall(bm)[0])
+        key = getKeyFromID(resID)
+    
+        infile = None
+        try:
+            infile = open(resID+'/'+resID+'.txt')
+        except IOError:
+            sys.stderr.write("WARNING: could not open file for experiment "+resID+"!\n")
+        
+        if infile != None:
+            if bm not in results:
+                results[bm] = {}
+
+            if useKey:
+                if key not in results:
+                    results[bm][key] = {}
+
+            res = ipcPattern.findall(infile.read())
+            for r in res:
+                resAr = r.split()
+                if len(res) == 1:
+                    assert not useKey
+                    results[bm] = float(resAr[1])
+                else:
+                    id = int(idPattern.findall(resAr[0])[0])
+                    bmName = fair_wls.workloads[wlNum][0][id]
+                    fw = fair_wls.workloads[wlNum][1][id]
+
+                    offset = fw - 1000000000
+                    bmName = bmName+str(offset / 20000000)
+
+                    if useKey:
+                        results[bm][key][bmName] = float(resAr[1])
+                    else:
+                        results[bm][bmName] = float(resAr[1])
+                    
+def getRatios(data, baseline, alone):
+    ratios = {}
+    for wl in data:
+        for key in data[wl]:
+            for bm in data[wl][key]:
+                if wl not in ratios:
+                    ratios[wl] = {}
+                if key not in ratios[wl]:
+                    ratios[wl][key] = {}
+
+                sharedIPC = data[wl][key][bm]
+                baselineIPC = -10000000.0
+                if alone:
+                    baselineIPC = baseline[bm]
+                else:
+                    baselineIPC = baseline[wl][bm]
+
+                ratios[wl][key][bm] = sharedIPC / baselineIPC
+
+    return ratios
+                
+
 
 staticIDs = []
 for cmd, config in pbsconfig.staticcommands:
@@ -42,6 +118,8 @@ for cmd, config in pbsconfig.alonecommands:
 
 
 interferenceRes = {}
+sharedIPCs = {}
+runIDs = []
 for cmd, config in pbsconfig.commandlines:
     resID = pbsconfig.get_unique_id(config)
     bm = getBenchmark(cmd)
@@ -55,6 +133,7 @@ for cmd, config in pbsconfig.commandlines:
             sys.stderr.write("WARNING: could not open file for experiment "+resID+"!\n")
         
         if infile != None:
+            runIDs.append(resID)
 
             if bm not in interferenceRes:
                 interferenceRes[bm] = {}
@@ -70,6 +149,15 @@ for cmd, config in pbsconfig.commandlines:
             interferenceRes[bm][key]["Mem"+str(i)+"Bus"] = getMatrix(infile)
             interferenceRes[bm][key]["Mem"+str(i)+"Con"] = getMatrix(infile)
             interferenceRes[bm][key]["Mem"+str(i)+"HtM"] = getMatrix(infile)
-            
-print interferenceRes
+   
+runIPCs = {}
+getIPCs(runIDs, runIPCs, True)
+staticIPCs = {}
+getIPCs(staticIDs, staticIPCs, False)
+aloneIPCs = {}
+getIPCs(aloneIDs, aloneIPCs, False)
+
+staticRatios = getRatios(runIPCs, staticIPCs, False)
+aloneRatios = getRatios(runIPCs, aloneIPCs, True)
+
 
