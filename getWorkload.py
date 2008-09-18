a33c6cc757eb5b045e2a2795558aec9008958a3d
jahre <devnull@localhost>
2008-09-18 06:56:05 +0000

    la til bw workloadene

--- getWorkload.py
+++ getWorkload.py
@@ -1,12 +1,15 @@
 
 import sys
 import workloads
+import bw_workloads
 import deterministic_fw_wls as fair_wls
 
 try:
 
     if sys.argv[1] == "regular":
         list = workloads.workloads[int(sys.argv[2])][int(sys.argv[3])][0]
+    elif sys.argv[1] == "bw":
+        list = bw_workloads.bw_workloads[int(sys.argv[2])][int(sys.argv[3])][0]
     elif sys.argv[1] == "fair":
         list = fair_wls.workloads[int(sys.argv[3])][0]
 
