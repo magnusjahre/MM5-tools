4e41d6ebb42187ef8bb7122617e060b5b766db34
jahre <devnull@localhost>
2008-11-04 16:11:53 +0000

    har implementert kopiering av single-exps også

--- copyResFiles.py
+++ copyResFiles.py
@@ -24,6 +24,33 @@ resultfiles = ["interconnect*.txt",
                "*InterferenceTrace.txt",
                "amhaTrace.txt"]
 
+def checkFile(filename, pattern, limit):
+    try:
+        tmpfile = open(filename)
+    except:
+        tmpfile = None
+
+    if tmpfile != None:
+        res = pattern.findall(tmpfile.read())
+        if res != []:
+            assert(len(res) == 1)
+            value = int(res[0].split()[1])
+            if value >= (limit * 0.99):
+                dirname,textfile = os.path.split(filename)
+                print "Copying results for exp "+dirname
+                curDest = resdir+"/"+dirname 
+                os.mkdir(curDest)
+                shutil.copy(filename, curDest)
+                for f in resultfiles:
+                    names = glob.glob(dirname+'/'+f)
+                    for name in names:
+                        shutil.copy(name, curDest)
+                return True
+
+    return False
+    
+
+
 try:
     simticks = int(sys.argv[1])
     resdir = sys.argv[2]
@@ -37,6 +64,8 @@ if not os.path.isdir(resdir):
     sys.exit()
 
 tickpattern = re.compile("sim_ticks.*")
+instpattern = re.compile("COM:count.*")
+
 
 print
 print "Starting result copy..."
@@ -45,43 +74,35 @@ print "Destination: "+resdir
 print
 
 resfiles = []
+alonefiles = []
 
 print "Retrieving result filenames.. ",
 
 for cmd, params in pbsconfig.commandlines:
     resID = pbsconfig.get_unique_id(params)
-    resfiles.append(resID+"/"+resID+".txt")
     
+    resfiles.append(resID+"/"+resID+".txt")
+
+    wl = pbsconfig.get_workload(params)
+    for i in range(pbsconfig.get_np(params)):
+        aloneparams = pbsconfig.get_alone_params(wl,i,params)
+        aloneid = pbsconfig.get_unique_id(aloneparams)
+        alonefiles.append(aloneid+"/"+aloneid+".txt")
 
 print "done!"
 
 
-while resfiles != []:
+while resfiles != [] or alonefiles != []:
     print "Checking for finished experiments..."
     rfcopy = list(resfiles)
     for rf in rfcopy:
+        if checkFile(rf, tickpattern, simticks):
+            resfiles.remove(rf)
 
-        try:
-            tmpfile = open(rf)
-        except:
-            tmpfile = None
-
-        if tmpfile != None:
-            res = tickpattern.findall(tmpfile.read())
-            if res != []:
-                ticks = int(res[0].split()[1])
-                if ticks >= (simticks * 0.99):
-                    dirname,filename = os.path.split(rf)
-                    print "Copying results for exp "+dirname
-                    curDest = resdir+"/"+dirname 
-                    os.mkdir(curDest)
-                    shutil.copy(rf, curDest)
-                    for f in resultfiles:
-                        names = glob.glob(dirname+'/'+f)
-                        for name in names:
-                            shutil.copy(name, curDest)
-                    
-                    resfiles.remove(rf)
+    alonecopy = list(alonefiles)
+    for af in alonecopy:
+        if checkFile(af, instpattern, 0):
+            alonefiles.remove(af)
 
 
     print "Sleeping"
