99e5a23ce5155a1f2344f213a67787d4b9b61135
jahre <devnull@localhost>
2009-09-01 15:04:41 +0000

    committer for å rydde opp

--- checkpoints/unittests/CheckpointMergeTest.py
+++ checkpoints/unittests/CheckpointMergeTest.py
@@ -1,22 +1,68 @@
 #!/usr/bin/python
 
 import unittest
+import os
+import deterministic_fw_wls as workloads
 from m5test.M5Command import M5Command
+import checkpoints
+from checkpoints.Checkpoint import Checkpoint
 
-
-class Test(unittest.TestCase):
-
+class CheckpointMergeTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        
+        self.wlName = "fair01"
+        self.np = 4
+        self.memsys = "RingBased"
+        
+        self.testWorkload = workloads.getBms(self.wlName, self.np)
+        fwInsts = 10000000
+        fwInstInc = 5000000
+        
+        print "Generating checkpoints for fair01"
+        
+        for bm in self.testWorkload:
+            
+            chkPath = checkpoints.getCheckpointDirectory(1, self.memsys, bm+"0")
+            if os.path.exists(chkPath):
+                print "Checkpoint allready exists for "+bm+", skipping..."
+                continue
+            
+            print "Generating checkpoint for "+bm+" with "+str(fwInsts)+" instructions in checkpoint"
+            self.generateCheckpoint(bm+"0", fwInsts)
+            fwInsts += fwInstInc
 
+    def generateCheckpoint(self, bm, fwinsts):
+        m5cmd = M5Command()
+        m5cmd.setUpTest(bm, 1, "RingBased", 1)
+        
+        m5cmd.setArgument("MEMORY-ADDRESS-OFFSET", 0)
+        m5cmd.setArgument("MEMORY-ADDRESS-PARTS", 4)
+        m5cmd.setArgument("MEMORY-BUS-SCHEDULER", "RDFCFS") 
+        m5cmd.setArgument("GENERATE-CHECKPOINT", "")
+        m5cmd.setArgument("SIMINSTS", fwinsts)
+        m5cmd.setExpectedComInsts(fwinsts)
+        
+        success = m5cmd.run(0, "simpleCPU.*num_insts.*")
+        self.assert_(success)
 
     def tearDown(self):
         pass
 
 
     def testCheckpointMerge(self):
-        pass
+        
+        wlCheckpoints = []
+        for bm in self.testWorkload:
+            checkPath = checkpoints.getCheckpointDirectory(1, self.memsys, bm+"0")
+            checkFile = checkPath+"/m5.cpt" 
+            
+            print "Reading checkpoint from file "+checkFile
+            
+            newCheckpoint = Checkpoint()
+            newCheckpoint.createFromFile(checkFile)
+            
+            wlCheckpoints.append(newCheckpoint)
 
 
 if __name__ == "__main__":
--- checkpoints/unittests/CheckpointTestCase.py
+++ checkpoints/unittests/CheckpointTestCase.py
@@ -4,59 +4,42 @@ import unittest
 from checkpoints.Checkpoint import Checkpoint
 from checkpoints.IniFileSection import IniFileSection
 
+from guppy import hpy
+import gc
+
 __metaclass__ = type
 
 class CheckpointTestCase(unittest.TestCase):
-
-    def testIniFileSection(self):
-        
-        rootSec = IniFileSection()
-        rootSec.setName("Root")
-        firstChild = IniFileSection()
-        firstChild.setName("Child 1")
-        secondChild = IniFileSection()
-        secondChild.setName("Child 2")
-
-        rootSec.addChild(firstChild)
-        rootSec.addChild(secondChild)
-        
-        self.assert_(not rootSec.isEmpty())
-        self.assert_(firstChild.isEmpty())
-        self.assert_(secondChild.isEmpty())
-        
-        firstChild.addDataElement("Testdata", "15")
-        self.assert_(not firstChild.isEmpty())
-        self.assert_(secondChild.isEmpty())
-        
-        secondChild.addDataElement("Testdata", "15")
-        self.assert_(not secondChild.isEmpty())
 
     def testSectionOrder(self):
         
         path = "/home/jahre/workspace/m5sim-tools/checkpoints/unittests/"
         
+        h = hpy()
+        h.setrelheap()
+        
+        outfilename = "vpr0-50000.cpt.out"
+        
         checkpoint1 = Checkpoint()
-        checkpoint1.createFromFile(path+"vpr0-50000.cpt")
+        checkpoint1.prepareOutputFile(outfilename)
+        checkpoint1.createFromFile(path+"vpr0-50000.cpt", outfilename , 3)
 
-        checkpoint1.writeToFile("vpr0-50000.cpt.out")
+        
+        heap = h.heap()
+        print heap        
+        
+        checkpoint1.writeToFile(outfilename)
         checkpoint2 = Checkpoint()
-        checkpoint2.createFromFile("vpr0-50000.cpt.out")
+        checkpoint2.createFromFile(outfilename, "vpr0-50000.cpt.mock", 3)
         
         chk1Sections = checkpoint1.sections
         chk2Sections = checkpoint2.sections
         
         self.assert_(len(chk1Sections) == len(chk2Sections))
-        self.traverseCheckpoints(chk1Sections, chk2Sections)
         
-    def traverseCheckpoints(self, sec1, sec2):
-        for name in sec1:
-            self.assert_(name in sec2)
-            for elem in sec1[name].dataElements:
-                self.assert_(elem in sec2[name].dataElements)
-                self.assertEquals(sec1[name].dataElements[elem], sec2[name].dataElements[elem])
-            
-            self.traverseCheckpoints(sec1[name].children, sec2[name].children)
-
+#        for i in range(len(chk1Sections)):
+#            self.assert_(chk1Sections[i].name == chk2Sections[i].name)
+#            self.assertEquals(chk)
 
 if __name__ == "__main__":
     unittest.main()
