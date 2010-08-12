e497ccc7b394017b5ed8ae79b88d9b246a3b52eb
jahre <devnull@localhost>
2010-08-12 11:13:17 +0000

    fjernet specrand

--- fairmha/experimentconfig.py
+++ fairmha/experimentconfig.py
@@ -4,7 +4,7 @@ import deterministic_fw_wls as workloads
 import copy
 
 specnames = ['gzip', 'vpr', 'gcc', 'mcf', 'crafty', 'parser', 'eon', 'perlbmk', 'gap', 'bzip', 'twolf', 'wupwise', 'swim', 'mgrid', 'applu', 'galgel', 'art', 'equake', 'facerec', 'ammp', 'lucas', 'fma3d', 'sixtrack' ,'apsi', 'mesa', 'vortex1']
-spec2006names = ['s6-bzip2', 's6-gcc', 's6-mcf', 's6-gobmk', 's6-hmmer', 's6-sjeng', 's6-libquantum', 's6-h264ref', 's6-omnetpp', 's6-astar', 's6-specrand', 's6-bwaves', 's6-gamess', 's6-milc', 's6-zeusmp', 's6-gromacs', 's6-cactusADM', 's6-leslie3d', 's6-namd', 's6-dealII', 's6-soplex', 's6-povray', 's6-calculix', 's6-gemsFDTD', 's6-tonto', 's6-lbm', 's6-sphinx3']
+spec2006names = ['s6-bzip2', 's6-gcc', 's6-mcf', 's6-gobmk', 's6-hmmer', 's6-sjeng', 's6-libquantum', 's6-h264ref', 's6-omnetpp', 's6-astar', 's6-bwaves', 's6-gamess', 's6-milc', 's6-zeusmp', 's6-gromacs', 's6-cactusADM', 's6-leslie3d', 's6-namd', 's6-dealII', 's6-soplex', 's6-povray', 's6-calculix', 's6-gemsFDTD', 's6-tonto', 's6-lbm', 's6-sphinx3']
 
 class ExperimentConfiguration:
     
