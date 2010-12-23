e8eaf27418b95253b9003435f36aa8f5bf9dca9b
jahre <devnull@localhost>
2010-12-23 08:41:14 +0000

    prøver meg med nye grenser da noen workloads gikk i taket med de gamle

--- fairmha/runconfig.py
+++ fairmha/runconfig.py
@@ -20,7 +20,7 @@ class ComputerParams:
         
         self.queue = opts.queue
         self.projectNum = None
-        self.perProcMem = {1:1984, 4:1984, 8:1984, 16:4032}
+        self.perProcMem = {1:2304, 4:2304, 8:2304, 16:4032}
         
         if re.search("stallo", compname):
             info("Stallo run detected...")
@@ -30,7 +30,7 @@ class ComputerParams:
         
         elif re.search("kongull", compname):
             info("Kongull run detected...")
-            self.ppn = {1:12, 4:12, 8:12, 16:6}
+            self.ppn = {1:10, 4:10, 8:10, 16:6}
             self.walltime = {1:10, 4:60, 8:168, 16:168}
             
         else:
