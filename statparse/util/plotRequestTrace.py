dc55c83628b2cafe5123782506b76027c7310233
jahre <devnull@localhost>
2012-02-07 21:22:48 +0000

    har laget dependencyanalyse

--- statparse/util/plotRequestTrace.py
+++ statparse/util/plotRequestTrace.py
@@ -10,7 +10,7 @@ class Request:
     
     def __init__(self, row):
         self.curTick = row[0]
-        self.id = row[1]
+        self.id = int(row[1])
         self.address = row[2]
         self.issuedAt = row[3]
         self.completedAt = row[4]
@@ -27,15 +27,29 @@ class Request:
             
         self.requestCausedStallAt = row[6]
         self.requestStallResumedAt = row[7]
-                       
+        
+        self.dependsOn = None
+        self.children = []
+        
+    def setDependsOn(self, req):
+        self.dependsOn = req
+        
+    def addChild(self, req):
+        self.children.append(req)
+        
+    def __str__(self):
+        return str(self.id)+" (issued at "+str(self.issuedAt)+", completed at "+str(self.completedAt)+")"
 
 def parseArgs():
     parser = OptionParser(usage="analyzeTrace.py [options] filename1")
 
+    plotTypes = ["requests", "heightbar", "heighthistogram"]
+
     parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False, help="Only write results to stdout")
     parser.add_option("-f", "--plot-from", action="store", type="int", dest="plotFrom", default=0, help="plot from this request id")
     parser.add_option("-s", "--plot-size", action="store", type="int", dest="plotSize", default=0, help="plot this number of requests")
-    parser.add_option("-t", "--plot-type", action="store", type="string", dest="plotType", default="requests", help="type of plot")
+    parser.add_option("-t", "--plot-type", action="store", type="string", dest="plotType", default="", help="type of plot, one of "+str(plotTypes))
+    parser.add_option("--avg-alone-lat", action="store", type="float", dest="avgAloneLat", default=0.0, help="average alone memory latency")
     
     opts, args = parser.parse_args()
     
@@ -44,10 +58,11 @@ def parseArgs():
         print "Usage: "+parser.usage
         sys.exit(-1)
     
-    plotTypes = ["requests", "heightbar", "heighthistogram"]
-    if opts.plotType not in plotTypes:
-        print "Plot type must be in "+str(plotTypes)
-        sys.exit(-1)
+    
+    if opts.plotType != "":
+        if opts.plotType not in plotTypes:
+            print "Plot type must be in "+str(plotTypes)
+            sys.exit(-1)
     
     return opts,args
 
@@ -121,7 +136,7 @@ def createHeightData(parareqs, requests):
     
     return height
 
-def getStats(requests, parareqs):
+def getStats(requests, parareqs, maxdepth, opts, stalls):
     
     totalLatency = 0.0
     totalStall = 0.0
@@ -141,8 +156,81 @@ def getStats(requests, parareqs):
     print "Total stall:       ", totalStall
     print "Stall per request: ", totalStall / numReqs
     print "T. issue to stall: ", totalIssueToStall
-    print "Overlap:           ", totalStall / totalLatency    
+    print "Overlap:           ", totalStall / totalLatency
+    print "Max. depth:        ", maxdepth
+    print "Avg st. per level: ", sum(stalls) / len(stalls)
     
+    if opts.avgAloneLat > 0:
+        print "Alone stall est.   ", opts.avgAloneLat*maxdepth
+
+def buildRequestGraph(requests):
+    
+    roots = []
+    
+    for i in range(len(requests)):
+        minDistance = 10000000000
+        minIndex = -1
+        
+        for j in range(len(requests)):
+            if j >= i:
+                break
+            
+            difference = requests[i].issuedAt - requests[j].completedAt
+            if difference > 0 and difference < minDistance:
+                minIndex = j
+                minDistance = difference
+        
+        assert requests[i].dependsOn == None        
+        if minIndex != -1:
+            requests[i].setDependsOn(requests[minIndex])
+            requests[minIndex].addChild(requests[i])
+        else:
+            roots.append(requests[i]) 
+    
+    return roots
+
+def makeDepencencyDot(roots):
+    
+    maxdepths = []
+    
+    dotfile = open("dependencies.dot", "w")
+    dotfile.write("digraph G{\n")
+    for r in roots:
+        maxdepths.append(traverseDependencies(r, dotfile, 0))
+    dotfile.write("}\n")
+    dotfile.flush()
+    dotfile.close()
+    
+    return max(maxdepths)
+    
+def traverseDependencies(node, dotfile, depth):
+    depth += 1
+    depths = []
+    for c in node.children:
+        if node.requestCausedStall:
+            dotfile.write(str(node.id)+" [color=red]")
+        dotfile.write(str(node.id)+" -> "+str(c.id)+" [label="+str(int(c.issuedAt-node.completedAt))+"]\n")
+        depths.append(traverseDependencies(c, dotfile, depth))
+    
+    if node.children == []:
+        return depth
+    return max(depths)
+
+def treeStalls(roots, maxdepth):
+    buffer = [0 for i in range(maxdepth)]
+    
+    for r in roots:
+        findStallPerTreeLevel(r, 0, buffer)
+        
+    return buffer
+    
+
+def findStallPerTreeLevel(node, depth, buffer):
+    if node.requestCausedStall:
+        buffer[depth] += node.requestStallResumedAt - node.requestCausedStallAt
+    
+    for c in node.children:
+        findStallPerTreeLevel(c, depth+1, buffer)
 
 def main():
 
@@ -181,19 +269,24 @@ def main():
         parareqs[pos].append(req)
         outstandingReqs[pos] = req
     
-    getStats(requests, parareqs)
     
-    if opts.plotType == "requests":
-        plotdata, colors = makePlotData(parareqs)
-        plot(plotdata, colors)
-    elif opts.plotType == "heightbar":
-        heightdata = createHeightData(parareqs, requests)
-        plotRawBarChart(heightdata)
-    elif opts.plotType == "heighthistogram":
-        heightdata = createHeightData(parareqs, requests)
-        plotHistogram(heightdata)
-    else:
-        print "Unknown plot type"
+    roots = buildRequestGraph(requests)
+    maxdepth = makeDepencencyDot(roots)
+    stalls = treeStalls(roots, maxdepth)
+    getStats(requests, parareqs, maxdepth, opts, stalls)
+    
+    if opts.plotType != "":
+        if opts.plotType == "requests":
+            plotdata, colors = makePlotData(parareqs)
+            plot(plotdata, colors)
+        elif opts.plotType == "heightbar":
+            heightdata = createHeightData(parareqs, requests)
+            plotRawBarChart(heightdata)
+        elif opts.plotType == "heighthistogram":
+            heightdata = createHeightData(parareqs, requests)
+            plotHistogram(heightdata)
+        else:
+            print "Unknown plot type"
         
         
 if __name__ == '__main__':
