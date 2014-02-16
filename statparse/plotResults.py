b4630bce97aa82678ff7b816818ad6e7412cf417
jahre <devnull@localhost>
2014-02-16 10:17:37 +0100

    added a rotate option and an option for making workload/benchmark keys more human readable

--- statparse/plotResults.py
+++ statparse/plotResults.py
@@ -779,7 +779,11 @@ def plotDataFileBarChart(names, values, legendNames, **kwargs):
     
     ax.set_xlim(0, len(names))
     ax.set_xticks(ind+(width/2.0))
-    ax.set_xticklabels(names, rotation="horizontal")
+    
+    if "rotate" in kwargs:
+        ax.set_xticklabels(names, rotation=kwargs["rotate"])
+    else:
+        ax.set_xticklabels(names, rotation="horizontal")
     
     plt.axhline(0, color='black')
     
