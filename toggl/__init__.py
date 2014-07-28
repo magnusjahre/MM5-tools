5d25fa70333086f43f0b331306c14e0dcc45f37b
jahre <devnull@localhost>
2014-07-28 11:18:32 +0200

    Implemented functions for detecting red days and half working days for a given year

--- toggl/__init__.py
+++ toggl/__init__.py
@@ -1,11 +1,37 @@
 import requests
 import ast
+from datetime import *
+from dateutil.easter import *
+from dateutil.relativedelta import *
 
 def togglRequest(baseurl, params):
     r = requests.get(baseurl, params=params, auth=('223867917969fce808959853d3776185', 'api_token'))
-    print r.url
     content = r.content
     content = content.replace("null", "0") #FIXME: not particularly safe, e.g. the string nullify
     data = ast.literal_eval(content)
     return data
 
+def getRedDays(year):
+    reddays = []
+    reddays.append(date(year, 1, 1))
+    reddays = reddays + getEasterRedDays(year)
+    reddays.append(date(year, 5, 1))
+    reddays.append(date(year, 12, 25))
+    reddays.append(date(year, 12, 26))
+    return reddays
+    
+def getHalfDays(year):
+    halfdays = []
+    easterSunday = easter(year)
+    halfdays.append(easterSunday-relativedelta(days=4))    
+    halfdays.append(date(year, 12, 24))
+    return halfdays
+
+def getEasterRedDays(year):
+    easterSunday = easter(year)
+    skjaertorsdag = easterSunday-relativedelta(days=3)
+    langfredag = easterSunday-relativedelta(days=2)
+    paaskedag = easterSunday+relativedelta(days=1)
+    himmelfart = easterSunday+relativedelta(days=39)
+    pinsedag = easterSunday+relativedelta(days=50)
+    return [skjaertorsdag, langfredag, paaskedag, himmelfart, pinsedag]
--- toggl/getWorkingHours.py
+++ toggl/getWorkingHours.py
@@ -2,23 +2,28 @@
 
 import sys
 from optparse import OptionParser
-from toggl import togglRequest
+from toggl import *
 
 def parseArgs():
     
-    parser = OptionParser(usage="getWorkingHours.py [options]")
+    parser = OptionParser(usage="getWorkingHours.py [options] year")
     #parser.add_option("--threads", '-t', action="store", dest="threads", default=4, type="int", help="Number of worker threads")
     opts, args = parser.parse_args()
     
-    if len(args) != 0:
+    if len(args) != 1:
         print "Usage:"
         print parser.usage
         sys.exit()
     
-    return opts, args
+    year = int(args[0])
+
+    return opts, year
 
 if __name__ == '__main__':
-    opts, args = parseArgs()
-    params = {"workspace_id": "626815", "user_agent": "api_test"}
-    data = togglRequest("https://toggl.com/reports/api/v2/weekly", params)
-    
+    opts, year = parseArgs()
+    #params = {"workspace_id": "626815", "user_agent": "api_test"}
+    #data = togglRequest("https://toggl.com/reports/api/v2/weekly", params)
+    redDays = getRedDays(year)
+    print redDays
+    halfDays = getHalfDays(year)
+    print halfDays
