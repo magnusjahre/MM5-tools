3afbaf92403608be74ee3e2d1e73fedabc355242
jahre <devnull@localhost>
2016-06-30 09:42:21 +0200

    Added support for checking private mode experiments

--- util/experimentStatus.py
+++ util/experimentStatus.py
@@ -24,33 +24,44 @@ def parseArgs():
     
     return opts, args, pbsconfig
 
+def processExperiment(params, pbsconfig, expectedCores, privmode):
+    expid = pbsconfig.get_unique_id(params)
+    if privmode:
+        cores = 1
+    else:
+        cores = pbsconfig.get_np(params)
+    expectedCores += cores
+    if cores == 1 and not privmode:
+        wl = pbsconfig.get_benchmark(params)
+    else:
+        wl = pbsconfig.get_workload(params)
+        
+    lines = 0
+    try:
+        dumpOrderFile = open(expid+"/statsDumpOrder.txt")
+        lines = len(dumpOrderFile.readlines())
+        dumpOrderFile.close()
+    except:
+        pass
+    
+    print wl.ljust(15)+expid.ljust(55)+str(lines)+" / "+str(cores)+(str((float(lines)*100)/float(cores))+"%").rjust(10)
+    return expectedCores, lines
+
 def main():
     commands, opts, pbsconfig = parseArgs()
 
     expectedCores = 0
     completedCores = 0
     
-    print "Experiment status:"
-    for cmd, params in pbsconfig.commandlines:
-        expid = pbsconfig.get_unique_id(params)
-        cores = pbsconfig.get_np(params)
-        expectedCores += cores
-        if cores == 1:
-            wl = pbsconfig.get_benchmark(params)
-        else:
-            wl = pbsconfig.get_workload(params)
+    #print "Experiment status:"
+    #for cmd, params in pbsconfig.commandlines:
+    #    expectedCores, lines = processExperiment(params, pbsconfig, expectedCores, False)
+    #    completedCores += lines
         
-        lines = 0
-        try:
-            dumpOrderFile = open(expid+"/statsDumpOrder.txt")
-            lines = len(dumpOrderFile.readlines())
-            dumpOrderFile.close()
-        except:
-            pass
-        
-        completedCores += lines
-
-        print wl.ljust(15)+expid.ljust(55)+str(lines)+" / "+str(cores)+(str((float(lines)*100)/float(cores))+"%").rjust(10)
+    if os.path.exists("pbsfiles-priv-mode"):
+        for cmd, params in pbsconfig.privModeCommandlines:
+            expectedCores, lines = processExperiment(params, pbsconfig, expectedCores, True)
+            completedCores += lines
     
     print "Summmary:", completedCores,"out of",expectedCores,"complete",
     print "("+("%.2f" % ((float(completedCores)*100)/float(expectedCores)))+"%)"
