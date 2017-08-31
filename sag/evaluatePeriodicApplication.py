f9b3fab82e9c01ae0a77085d8968dc71868da530
Magnus Jahre <magnus.jahre@idi.ntnu.no>
2017-08-31 11:15:14 +0200

    Model shape now makes sense
    Getting the balance right between ALPHA-S and the clock frequency is
    critical to observe the minimum energy point.

--- sag/evaluatePeriodicApplication.py
+++ sag/evaluatePeriodicApplication.py
@@ -5,11 +5,13 @@ from optparse import OptionParser
 from sag import fatal
 
 from statparse.plotResults import plotRawLinePlot
+from statparse.printResults import printData, numberToString
 
 def parseArgs():
     parser = OptionParser(usage="evaluatePeriodicApplication.py [options] model-file")
 
     parser.add_option("--verbose", action="store_true", dest="verbose", default=False, help="Enable verbose output")
+    parser.add_option("--decimals", action="store", dest="decimals", type="int", default=2, help="Number of decimals to use when printing results")
     
     opts, args = parser.parse_args()
 
@@ -50,23 +52,51 @@ def readModelFile(modelfilename):
     assert "OP" not in model
     model["OP"] = operatingPoints
     
-    print model
     return model
 
 def estimateEnergy(voltage, frequency, model, opts):
     
-    pDyn = model["ALPHA-D"]*model["PERIOD-INSTRUCTIONS"]*voltage**2
+    eDyn = model["ALPHA-D"]*model["PERIOD-INSTRUCTIONS"]*voltage**2
     if opts.verbose:
-        print "alpha-d="+str(model["ALPHA-D"])+", instructions="+str(model["PERIOD-INSTRUCTIONS"])+", voltage="+str(voltage)+" gives Pd="+str(pDyn)
+        print "alpha-d="+str(model["ALPHA-D"])+", instructions="+str(model["PERIOD-INSTRUCTIONS"])+", voltage="+str(voltage)+" gives Pd="+str(eDyn)
     
-    pStat = (model["ALPHA-S"]*model["PERIOD-INSTRUCTIONS"]*voltage)/float(frequency)
+    eStat = (model["ALPHA-S"]*model["PERIOD-INSTRUCTIONS"]*voltage)/float(frequency)
     
     if opts.verbose:
-        print "alpha-s="+str(model["ALPHA-S"])+", f="+str(frequency)+" gives Ps="+str(pStat)
+        print "alpha-s="+str(model["ALPHA-S"])+", I="+str(model["PERIOD-INSTRUCTIONS"])+", f="+str(frequency)+" gives Ps="+str(eStat)
     
-    eTot = pDyn + pStat
+    return eDyn, eStat
+
+def printEnergyData(vRange, data, opts):
     
-    return eTot
+    lines = []
+    header = [""]
+    justify = [True]
+    for v in vRange:
+        header.append(numberToString(v, opts.decimals))
+        justify.append(False)
+    lines.append(header)
+    
+    for series in data:
+        line = [series[0]]
+        for d in series[1:]:
+            line.append(numberToString(d, opts.decimals))
+        lines.append(line)
+    
+    print
+    print "Estimated Energy:"
+    print
+    printData(lines, justify, sys.stdout, opts.decimals)
+
+def findMinEP(vRange, Etot):
+    Emin = Etot[0]
+    Vopt = vRange[0]
+    for i in range(len(Etot))[1:]:
+        if Etot[i] < Emin:
+            Emin = Etot[i]
+            Vopt = vRange[i]
+    
+    return Emin,Vopt
 
 def main():
     args, opts = parseArgs()
@@ -78,10 +108,30 @@ def main():
         vRange.append(v)
         energy.append(estimateEnergy(v, f, model, opts))
     
-    print vRange
-    print energy
+    Edyn = []
+    Estat = []
+    Etot = []
+    unitFactor = 1000
+    for d,s in energy:
+        Edyn.append(d*unitFactor)
+        Estat.append(s*unitFactor)
+        Etot.append((d+s)*unitFactor)
     
-    plotRawLinePlot(vRange, [energy])
+    printEnergyData(vRange, [["Edyn"]+Edyn, ["Estat"]+Estat, ["Etot"]+Etot], opts)
+    
+    Emin, Vopt = findMinEP(vRange, Etot)
+    print
+    print "Optimal Voltage is "+str(Vopt)
+    
+    plotRawLinePlot(vRange,
+                    [Edyn, Estat, Etot],
+                    titles=["$E_{dyn}$","$E_{stat}$","$E_{tot}$"],
+                    legendColumns=3,
+                    mode="None",
+                    xlabel="$V_{dd}$",
+                    ylabel="mJ",
+                    separators=str(Vopt),
+                    labels=str(Vopt*1.01)+","+str(Emin*1.2)+",Optimal Voltage")
 
 if __name__ == '__main__':
     main()
