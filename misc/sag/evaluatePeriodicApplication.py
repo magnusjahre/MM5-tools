63d17d9968b0232497d54a08b6fe86ac6c02d7ee
Magnus Jahre <magnus.jahre@idi.ntnu.no>
2018-04-19 16:13:51 +0200

    Package import fix after the reorganization

--- misc/sag/evaluatePeriodicApplication.py
+++ misc/sag/evaluatePeriodicApplication.py
@@ -2,7 +2,7 @@
 
 import sys
 from optparse import OptionParser
-from sag import fatal
+from misc.sag import fatal
 
 from statparse.plotResults import plotRawLinePlot, plotRawScatter
 from statparse.printResults import printData, numberToString
